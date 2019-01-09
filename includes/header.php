e44c6f836da91d8f5f695d43911b3374fc84def5
Elias <dta2@protonmail.com>
2019-01-09 09:41:57 +0100

    modification de la navbar avec l'ajout de la modal de connexion et la modal d'inscription

--- includes/header.php
+++ includes/header.php
@@ -85,3 +85,137 @@
 			</div>
 		</nav>
 	</header>
+
+	<!-- modal de connexion -->
+
+<div class="modal fade" id="connexion" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="container">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header text-white" style="background-color: #5F382C">
+        <h5 class="modal-title" id="exampleModalLabel"><strong></strong>Connectez-vous à votre compte Listar</strong></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+
+    <div class="modal-body"><br>
+        <form>
+            <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-10">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">             
+                </div>
+            </div>
+
+            <div class="form-group row">
+                    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+                <div class="col-sm-10">
+                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                </div>
+            </div><br>
+
+            <div class="form-group row">
+                <div class="col-sm-10">
+                    <button type="submit" class="btn btn-primary"><a id="connected" href="todolist">Connexion</a></button>
+                </div>
+            </div></a>
+        </form>
+    </div>
+        <div class="container">
+            <div class="row ml-auto">
+                <div class="col-1"></div>
+                <div class="col-10 modal-footer ">
+                <p>Vous n'avez pas de compte ? <a href="#inscript">S’inscrire &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></p>
+                <div class="col-1"></div>
+            </div>
+        </div>
+              
+          </div>
+      </div>
+      
+    </div>
+  </div>
+</div>
+<!-- fin de modal de connexion -->
+
+<!-- début de modal d'inscription -->
+<div class="modal fade" id="inscription" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header text-white" style="background-color: #5F382C">
+        <h5 class="modal-title" id="exampleModalLabel">Inscrivez-vous et ouvrez un compte Listar</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+
+        <!-- Formulaire d'inscription -->
+        <form>
+          <div class="form-row">
+            <div class="form-group col-md-6">
+              <label for="inputEmail4">Adresse Mail</label>
+              <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+          </div>
+          <div class="form-group col-md-6">
+              <label for="inputPassword4">Mot de passe</label>
+              <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+          </div>
+      </div>
+      <div class="form-group">
+        <label for="inputAddress">Addresse</label>
+        <input type="text" class="form-control" id="inputAddress" placeholder="7 rue de la marne">
+    </div>
+    <div class="form-group">
+        <label for="inputAddress2">Addresse 2</label>
+        <input type="text" class="form-control" id="inputAddress2" placeholder="Apartement, studio...">
+    </div>
+    <div class="form-row">
+        <div class="form-group col-md-6">
+            <label for="inputCity">Ville</label>
+          <input type="text" class="form-control" id="inputCity">
+      </div>
+      <div class="form-group col-md-4">
+          <label for="inputState">Pays</label>
+          <select id="inputState" class="form-control">
+            <option selected>France</option>
+            <option>Belgique</option>
+            <option>Suisse</option>
+            <option>Espagne</option>
+            <option>Portugal</option>
+            <option>Angleterre</option>
+            <option>...</option>
+        </select>
+    </div>
+    <div class="form-group col-md-2">
+      <label for="inputZip">zip</label>
+      <input type="text" class="form-control" id="inputZip">
+  </div>
+</div>
+
+<button type="submit" class="btn btn-primary">Inscription</button>
+</form>
+
+      </div>
+
+<!-- Fin du Formulaire d'inscription -->
+
+      <!-- <div class="col modal-footer text-center">
+        <p></p><br>
+        <p>Vous avez déjà un compte ? <a href="">Se connecter</a></p>
+      </div> -->
+        <div class="container">
+            <div class="row ml-auto">
+                <div class="col-md-1"></div>
+                <div class="col-md10 modal-footer">
+                <p>Vous avez déjà un compte ? <a href="#connexion">Connexion &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></p>
+                </div>
+                <div class="col-md-1"></div>
+            </div>
+              
+        </div>
+    </div>
+  </div>
+</div>
+<!-- fin de modal d'inscription -->
